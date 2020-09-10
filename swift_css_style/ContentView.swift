8267f861db7fcb59c356c70dd461cf77d782351f
AdonisHan <30692767+AdonisHan@users.noreply.github.com>
2020-09-11 00:41:47 +0900

    css_style
    1st

---
+++ swift_css_style/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_css_style
+//
+//  Created by Adonis Han on 9/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_css_style/swift_css_styleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_css_styleApp.swift
+//  swift_css_style
+//
+//  Created by Adonis Han on 9/11/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_css_styleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
